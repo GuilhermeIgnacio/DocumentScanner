5f74101d79d6369da5f0dcb1c729098aef5cdf2c
Guilherme Ignácio <stormierearth9@gmail.com>
2024-06-27 23:18:23 -0300

    moved AppScreen to a different file

--- app/src/main/java/com/guilherme/documentscanner/MainActivity.kt
+++ app/src/main/java/com/guilherme/documentscanner/MainActivity.kt
@@ -11,44 +11,12 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.core.net.toUri
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil.compose.AsyncImage
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_PDF
@@ -58,7 +26,7 @@ import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
 import com.guilherme.documentscanner.di.initKoin
 import com.guilherme.documentscanner.presentation.MainViewModel
 import com.guilherme.documentscanner.presentation.MainViewModelEvents
-import com.guilherme.documentscanner.presentation.components.DocumentDetailSheet
+import com.guilherme.documentscanner.presentation.components.AppScreen
 import com.guilherme.documentscanner.ui.theme.AppTheme
 import org.koin.androidx.compose.koinViewModel
 
@@ -134,20 +102,20 @@ class MainActivity : ComponentActivity() {
                                             outputStream.close()
                                             Toast.makeText(
                                                 this@MainActivity,
-                                                "PDF salvo em: $uri",
+                                                getString(R.string.pdf_stored_at, uri),
                                                 Toast.LENGTH_LONG
                                             ).show()
                                         } ?: run {
                                             Toast.makeText(
                                                 this@MainActivity,
-                                                "Erro ao salvar PDF: OutputStream nulo",
+                                                getString(R.string.error_storing_pdf),
                                                 Toast.LENGTH_LONG
                                             ).show()
                                         }
                                     } ?: run {
                                         Toast.makeText(
                                             this@MainActivity,
-                                            "Erro ao salvar PDF: URI nulo",
+                                            getString(R.string.error_storing_pdf),
                                             Toast.LENGTH_LONG
                                         ).show()
                                     }
@@ -161,88 +129,13 @@ class MainActivity : ComponentActivity() {
                     }
                 )
 
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .statusBarsPadding()
-                            .navigationBarsPadding()
-                    ) {
-
-                        LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                            items(state.data) {
-
-                                Surface(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(8.dp)
-                                        .clickable {
-                                            onEvent(MainViewModelEvents.OnDocumentClick(it))
-                                        },
-                                    shadowElevation = 16.dp,
-                                    shape = RoundedCornerShape(10)
-                                ) {
-                                    Row(
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(16.dp),
-                                        verticalAlignment = Alignment.CenterVertically,
-                                    ) {
-                                        AsyncImage(
-                                            modifier = Modifier
-                                                .width(100.dp)
-                                                .height(150.dp)
-                                                .clip(RoundedCornerShape(10)),
-                                            model = it.uri[0].toUri(),
-                                            contentDescription = null,
-                                            contentScale = ContentScale.Crop
-                                        )
-                                        Spacer(modifier = Modifier.weight(1f))
-                                        Icon(
-                                            imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
-                                            contentDescription = null,
-                                        )
-                                    }
-                                }
-
-
-                            }
-                        }
-
-                        FloatingActionButton(
-                            modifier = Modifier
-                                .align(Alignment.BottomCenter)
-                                .size(80.dp),
-                            onClick = {
-                                scanner.getStartScanIntent(this@MainActivity)
-                                    .addOnSuccessListener { intentSender ->
-                                        scannerLauncher.launch(
-                                            IntentSenderRequest.Builder(
-                                                intentSender
-                                            ).build()
-                                        )
-                                    }
-                                    .addOnFailureListener {
-                                        println(it)
-                                    }
-                            },
-                            shape = CircleShape,
-                            contentColor = Color.White
-                        ) {
-                            Icon(
-                                modifier = Modifier.size(32.dp),
-                                painter = painterResource(id = R.drawable.outline_scan_24),
-                                contentDescription = stringResource(R.string.scan_icon_desc)
-                            )
-                        }
-                    }
-                }
-
-                DocumentDetailSheet(state = state, onEvent = onEvent)
+                AppScreen(
+                    state = state,
+                    onEvent = onEvent,
+                    scanner = scanner,
+                    scannerLauncher = scannerLauncher,
+                    context = this@MainActivity
+                )
 
             }
 
@@ -250,4 +143,3 @@ class MainActivity : ComponentActivity() {
     }
 
 }
-
---
+++ app/src/main/java/com/guilherme/documentscanner/presentation/components/AppScreen.kt
@@ -0,0 +1,135 @@
+package com.guilherme.documentscanner.presentation.components
+
+import android.app.Activity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
+import coil.compose.AsyncImage
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanner
+import com.guilherme.documentscanner.R
+import com.guilherme.documentscanner.presentation.MainViewModelEvents
+import com.guilherme.documentscanner.presentation.MainViewState
+
+@Composable
+fun AppScreen(
+    state: MainViewState,
+    onEvent: (MainViewModelEvents) -> Unit,
+    scanner: GmsDocumentScanner,
+    scannerLauncher: ManagedActivityResultLauncher<IntentSenderRequest, ActivityResult>,
+    context: Activity
+) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+                .navigationBarsPadding()
+        ) {
+
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                items(state.data) {
+
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .clickable {
+                                onEvent(MainViewModelEvents.OnDocumentClick(it))
+                            },
+                        shadowElevation = 16.dp,
+                        shape = RoundedCornerShape(10)
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            AsyncImage(
+                                modifier = Modifier
+                                    .width(100.dp)
+                                    .height(150.dp)
+                                    .clip(RoundedCornerShape(10)),
+                                model = it.uri[0].toUri(),
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop
+                            )
+                            Spacer(modifier = Modifier.weight(1f))
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
+                                contentDescription = null,
+                            )
+                        }
+                    }
+
+
+                }
+            }
+
+            FloatingActionButton(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .size(80.dp),
+                onClick = {
+                    scanner.getStartScanIntent(context)
+                        .addOnSuccessListener { intentSender ->
+                            scannerLauncher.launch(
+                                IntentSenderRequest.Builder(
+                                    intentSender
+                                ).build()
+                            )
+                        }
+                        .addOnFailureListener {
+                            println(it)
+                            //Todo: Add an error message
+                        }
+                },
+                shape = CircleShape,
+                contentColor = Color.White
+            ) {
+                Icon(
+                    modifier = Modifier.size(32.dp),
+                    painter = painterResource(id = R.drawable.outline_scan_24),
+                    contentDescription = stringResource(R.string.scan_icon_desc)
+                )
+            }
+        }
+    }
+
+    DocumentDetailSheet(state = state, onEvent = onEvent)
+}
